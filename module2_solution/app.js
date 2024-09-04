64684e10d8c9c97302bad5ff40b485f14fc67951
Timour Melikov <timour.melikov@gmx.net>
2024-09-04 12:05:10 +0200

    update angula part

--- module2_solution/app.js
+++ module2_solution/app.js
@@ -2,4 +2,19 @@
     'use strict';
 
     angular.module('ShoppingListCheckOff')
-})
+    .controller('ToBuyController', ToBuyController)
+    .controller('AlreadyBoughtController', AlreadyBoughtController);
+
+    ToBuyController.$inject=['$scope'];
+
+    AlreadyBoughtController.$inject=['$scope'];
+
+    function ToBuyController($scope){
+
+    };
+
+    function AlreadyBoughtController($scope){
+
+    };
+
+})();
