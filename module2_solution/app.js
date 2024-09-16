f8f6aa92c95c8755fa730942760cea8c0725744f
Timour Melikov <timour.melikov@gmx.net>
2024-09-16 11:35:17 +0200

    final version

--- module2_solution/app.js
+++ module2_solution/app.js
@@ -1,7 +1,7 @@
 (function(){
     'use strict';
 
-    angular.module('ShoppingListCheckOff')
+    angular.module('ShoppingListCheckOff', [])
     .controller('ToBuyController', ToBuyController)
     .controller('AlreadyBoughtController', AlreadyBoughtController)
     .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
@@ -9,11 +9,12 @@
     ToBuyController.$inject=['ShoppingListCheckOffService'];
     
     function ToBuyController(ShoppingListCheckOffService){
-      toBuy = this;
+      var toBuy = this;
       
       toBuy.items = ShoppingListCheckOffService.getAvailableItems();
       
       toBuy.moveInBusket = function(itemIndex){
+        console.log("move in busket.");
         ShoppingListCheckOffService.moveInBoughtList(itemIndex);
       }
     };
@@ -21,37 +22,38 @@
     AlreadyBoughtController.$inject=['ShoppingListCheckOffService'];
 
     function AlreadyBoughtController(ShoppingListCheckOffService){
-      alreadyBought = this;
+      var alreadyBought = this;
       
       alreadyBought.boughtItems = ShoppingListCheckOffService.getBoughtItems();
-      
-      alreadyBought.moveItemBack = function(itemIndex){
-        ShoppingListCheckOffService.removeFromBuscket(itemIndex);
-      }
     };
 
     function ShoppingListCheckOffService(){
       var service = this;
 
-      var toBuyItems = ["Cookie","Cheaps","Sugar-Drinks"];
+      var toBuyItems = ["Cookie","Cheaps","Pizza", "Beer", "Fizzy drinks", "Chocolate"];
 
       var toBoughtItems =[];
+
+      service.getAvailableItems = function(){
+        console.log(toBuyItems);
+        return toBuyItems;
+      }
       
+
+
       service.moveInBoughtList = function(itemIndex)
       {
+
         var item = toBuyItems[itemIndex];
 
         toBoughtItems.push(item);
 
         toBuyItems.splice(itemIndex,1);
+
       }
 
-      service.removeFromBuscket = function(itemIndex){
-        var item =toBoughtItems[itemIndex]
-
-        toBuyItems.push(item);
-
-        toBoughtItems.splice(itemIndex, 1);
+      service.getBoughtItems = function(){
+        return toBoughtItems;
       }
     }
 
