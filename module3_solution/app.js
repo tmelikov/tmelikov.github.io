1e9df9fd1375af64025d320bd2c9d8a3b325219e
Timour Melikov <timour.melikov@gmx.net>
2024-09-19 19:09:37 +0200

    implemented partly step 5

--- module3_solution/app.js
+++ module3_solution/app.js
@@ -3,23 +3,63 @@
 
     angular.module('NarrowItDownApp',[])
     .controller('NarrowItDownController', NarrowItDownController)
-    .service('MenuSearchService', MenuSearchService);
+    .service('MenuSearchService', MenuSearchService)
+    .constant('MenuApiPath', "https://coursera-jhu-default-rtdb.firebaseio.com")
+    .directive('foundItems', FoundItemsDirective);
+
+    function FoundItemsDirective(){
+        var ddo ={
+            templateUrl: 'foundItems.html',
+            scope: {
+                foundItems: '<',
+                onRemove: '&'
+            }
+        };
+        return ddo;
+    }
     
     NarrowItDownController.$inject= ['MenuSearchService'];
     
     function NarrowItDownController(MenuSearchService){
 
-        var narroCtr = this;
+        var narrowCtr = this;
+
+        narrowCtr.searchTerm ="";
+
+        narrowCtr.found = MenuSearchService.getMatchedMenuItems(narrowCtr.searchTerm);
+
+        narrowCtr.removeItem = function(itemIndex){
+            console.log("remove item: ", itemIndex);
+        };
+
     };
 
-    function MenuSearchService(){
 
-        service = this;
+    MenuSearchService.$inject['$http', 'MenuApiPath']
+    function MenuSearchService($http, MenuApiPath){
 
-        this.getMatchedMenuItems = function (seatchTerm){
-            return [];
+        var service = this;
+
+        service.foundItems = [];
+
+        this.getMatchedMenuItems = function (searchTerm){
+
+            $http({
+                method: "GET",
+                url: (MenuApiPath + "/menu_items.json")
+            })
+            .then(function(response){
+                console.log("got data.");
+                service.foundItems = response.data;
+
+            })
+            .catch(function(error){
+                console.log(error)
+            });
+
+            
+
+            return service.foundItems;;
         }
     }
-
-
 })();
